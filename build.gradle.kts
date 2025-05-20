0e772080acbaf113831984467f43a3fa805b6cfb
Sebastian Barrasa <sbarrasa@superbid.com.ar>
2025-05-20 04:37:00 -0300

    gradle Exposed

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,8 +4,11 @@ plugins {
     kotlin("jvm") version "1.9.22"
 }
 
-group = "com.sbarrasa"
-version = "1.0-SNAPSHOT"
+val group = "com.sbarrasa"
+val version = "1.0-SNAPSHOT"
+val exposedVersion = "0.50.1"
+val h2Version = "2.2.224"
+
 
 repositories {
     mavenCentral()
@@ -15,6 +18,11 @@ dependencies {
     testImplementation(platform("org.junit:junit-bom:5.10.2"))
     testImplementation("org.junit.jupiter:junit-jupiter")
     testImplementation(kotlin("test"))
+    implementation("org.jetbrains.exposed:exposed-core:$exposedVersion")
+    implementation("org.jetbrains.exposed:exposed-dao:$exposedVersion")
+    implementation("org.jetbrains.exposed:exposed-jdbc:$exposedVersion")
+    implementation("org.jetbrains.exposed:exposed-kotlin-datetime:$exposedVersion")
+    implementation("com.h2database:h2:$h2Version")
 }
 
 tasks.test {
