ae09d0a82e9583c609b0d9e288e08942905b40ff
Sebastian Barrasa <sbarrasa@superbid.com.ar>
2025-11-23 18:50:28 -0300

    exposed Databasedriver cargado desde application.conf

---
+++ src/main/kotlin/com/bank/application/codesRoutes.kt
@@ -0,0 +1,28 @@
+package com.bank.application
+
+import com.bank.services.CodesCatalog
+import io.ktor.server.application.*
+import io.ktor.server.plugins.*
+import io.ktor.server.response.*
+import io.ktor.server.routing.*
+
+fun Application.codesRoutes() {
+   routing {
+      route("/codes") {
+         get("/all") {
+            call.respond(CodesCatalog)
+         }
+
+         get("/") {
+            call.respond(CodesCatalog.keys)
+         }
+
+         get("/{code}") {
+            val code = call.parameters["code"]
+            val map = CodesCatalog[code] ?: throw NotFoundException("Code $code not found")
+            call.respond(map)
+         }
+      }
+   }
+}
+
