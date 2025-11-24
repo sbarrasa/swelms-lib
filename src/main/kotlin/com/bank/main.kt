637a09b78ab7be3310fee575951066a64db70a21
Sebastian Barrasa <sbarrasa@superbid.com.ar>
2025-11-24 17:04:58 -0300

    module

--- src/main/kotlin/com/bank/main.kt
+++ src/main/kotlin/com/bank/main.kt
@@ -1,13 +1,6 @@
 package com.bank
 
-import com.bank.ktor.routes.codesRoutes
-import com.bank.ktor.routes.customerProductsRoutes
-import com.bank.ktor.routes.customerRoutes
-import com.bank.ktor.config.configDatabase
-import com.bank.ktor.config.configLocale
-import com.bank.ktor.config.configLog
-import com.bank.ktor.config.configSerialization
-import com.bank.ktor.config.configStatusPages
+import com.bank.ktor.configModule
 import com.typesafe.config.ConfigFactory
 import io.ktor.server.config.HoconApplicationConfig
 import io.ktor.server.engine.*
@@ -18,14 +11,7 @@ fun main(args: Array<String>) {
    val env = applicationEngineEnvironment {
       config = HoconApplicationConfig(ConfigFactory.load("application.conf"))
       module {
-         configLocale()
-         configStatusPages()
-         configSerialization()
-         configDatabase()
-         customerRoutes()
-         codesRoutes()
-         customerProductsRoutes()
-         configLog()
+         configModule()
       }
       connector {
          port = 8080
