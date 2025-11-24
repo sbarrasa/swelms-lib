8e63526d7a53a718b7043a80e6226ab0e0ded57a
Sebastian Barrasa <sbarrasa@superbid.com.ar>
2025-11-24 17:05:19 -0300

    CustomersTable.kt mejoras

--- src/main/kotlin/com/bank/database/CustomersTable.kt
+++ src/main/kotlin/com/bank/database/CustomersTable.kt
@@ -1,10 +1,13 @@
 package com.bank.database
 
 import com.bank.model.customer.Gender
-import org.jetbrains.exposed.dao.id.IntIdTable
+import org.jetbrains.exposed.sql.Table
 import org.jetbrains.exposed.sql.kotlin.datetime.date
 
-object CustomersTable : IntIdTable("customers") {
+object CustomersTable : Table("customers") {
+   val id = integer("id").autoIncrement()
+   override val primaryKey = PrimaryKey(id)
+
    val legalName = varchar("legal_name", 50)
    val cuit = varchar("cuit", 11)
    val birthDay = date("birth_day").nullable()
