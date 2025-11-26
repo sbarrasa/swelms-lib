99e4bbd380e00222e9d5aca12d61f260cb07a5d8
Sebastian Barrasa <sbarrasa@superbid.com.ar>
2025-11-26 12:22:07 -0300

    rating

---
+++ src/main/kotlin/com/swelms/domain/rate/Rating.kt
@@ -0,0 +1,24 @@
+package com.swelms.domain.rate
+
+import com.swelms.common.locale.*
+import kotlin.math.round
+
+class Rating(
+   private var sum: Int = 0,
+   private var count: Int = 0,
+   private val range: IntRange = 1..10
+) {
+   fun add(value: Int) {
+      require(value in range) { localeText["OUT_OF_RANGE"](range.first, range.last) }
+      sum += value
+      count++
+   }
+
+   fun averageRounded() = round(average()).toInt()
+
+   fun average() = if (count > 0) sum.toDouble() / count else 0.0
+
+   fun count() = count
+
+   fun sum() = sum
+}
---
+++ src/test/kotlin/com/swelms/domain/rate/RatingTest.kt
@@ -0,0 +1,37 @@
+package com.swelms.domain.rate
+
+import kotlin.test.*
+
+class RatingTest {
+   @Test
+   fun average1() {
+      val rating = Rating()
+      rating.add(5)
+      rating.add(2)
+      rating.add(3)
+
+      assertEquals(3, rating.averageRounded())
+   }
+
+   @Test
+   fun average2() {
+      val rating = Rating(10,2)
+      rating.add(5)
+
+      assertEquals(15, rating.sum())
+      assertEquals(3, rating.count())
+
+      assertEquals(5.0, rating.average())
+   }
+
+   @Test
+   fun invalidRange(){
+      val rating = Rating(range = 1..5)
+      assertFailsWith<IllegalArgumentException> {  rating.add(10)}
+
+
+   }
+
+
+
+}
