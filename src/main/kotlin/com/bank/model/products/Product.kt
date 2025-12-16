427e12116fb12da3c4213815930cb49087cca26e
Sebastian Barrasa <sbarrasa@superbid.com.ar>
2025-12-16 20:44:53 -0300

    simplificacions de productos

--- src/main/kotlin/com/bank/model/products/Product.kt
+++ src/main/kotlin/com/bank/model/products/Product.kt
@@ -1,25 +1,14 @@
 package com.bank.model.products
 
-import com.swelms.common.reflection.finalSubclasses
+import com.swelms.common.reflection.*
+import com.swelms.common.serialization.serialName
 import kotlinx.serialization.Serializable
 import kotlin.reflect.KClass
 import kotlin.reflect.full.companionObjectInstance
-import kotlin.reflect.full.primaryConstructor
 
 @Serializable
 sealed interface Product {
    val description: String
 
-   companion object {
-      val types get() = Product::class.finalSubclasses
-
-      inline fun <reified T : Product> create(map: Map<String, Any?>): T? {
-         val constructor = T::class.primaryConstructor ?: return null
-         val args = constructor.parameters.associateWith { map[it.name] }
-         return constructor.callBy(args)
-      }
-   }
-
+   val descriptor get() = ProductTypes[this::class]
 }
-
-val KClass<out Product>.descriptor get() = this.companionObjectInstance as ProductDescriptor
