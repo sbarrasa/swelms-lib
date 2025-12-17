b719f848ba68d31e3567b81f3590b4b2c86afd18
Sebastian Barrasa <sbarrasa@superbid.com.ar>
2025-12-17 16:25:49 -0300

    Result/Error framework java integration

--- src/main/kotlin/com/swelms/common/error/Result.kt
+++ src/main/kotlin/com/swelms/common/error/Result.kt
@@ -6,10 +6,10 @@ sealed class Result<T>(open val value: T?, open val error: Throwable?) {
    data class Error<T>(override val value: T?=null, override val error: Throwable) : Result<T>(value, error)
 
    infix fun orElse(block: (Result<T>) -> T): T = value ?: block(this)
-   infix fun orElse(defaultValue: T) = orElse {defaultValue}
+   infix fun orElse(defaultValue: T) = orElse { defaultValue }
 
    infix fun orAny(block: (Result<T>) -> Any?): Any? = value ?: block(this)
-   infix fun orAny(defaultValue: Any?) = orAny {defaultValue}
+   infix fun orAny(defaultValue: Any?) = orAny { defaultValue }
 
    infix fun onError(block: (Throwable) -> Unit): Result<T> {
       error?.let(block)
--- src/main/kotlin/com/swelms/common/error/Try.kt
+++ src/main/kotlin/com/swelms/common/error/Try.kt
@@ -1,8 +1,8 @@
 package com.swelms.common.error
 
 object Try{
-   @JvmStatic infix fun <T> get(block: () -> T) = tryGet(block)
-   @JvmStatic infix fun run(block: () -> Unit) = tryRun(block)
+   @JvmStatic fun <T> get(block: () -> T) = tryGet(block)
+   @JvmStatic fun run(block: () -> Unit) = tryRun(block)
 }
 
 fun <T> tryGet(block: () -> T): Result<T?> =
