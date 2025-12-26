94f965559a83a30e4a2ac75c2008680d1e46feb3
Sebastian Barrasa <sbarrasa@superbid.com.ar>
2025-12-25 21:21:11 -0300

    simplificación de EnumMap

--- src/main/kotlin/com/swelms/common/collections/EnumMap.kt
+++ src/main/kotlin/com/swelms/common/collections/EnumMap.kt
@@ -1,16 +1,18 @@
 package com.swelms.common.collections
 
+import kotlin.enums.EnumEntries
 
 class EnumMap<E : Enum<E>, V> (private val map: Map<E, V>) : Map<E, V> by map {
    val enumClass = map.keys.first()::class
 
-   companion object {
-      inline operator fun <reified E : Enum<E>, V> invoke(mapper: (E) -> V) =
-         EnumMap(enumValues<E>().associateWith(mapper))
-   }
+   constructor(
+      entries: Iterable<E>,
+      mapper: (E) -> V
+   ) : this(entries.associateWith(mapper))
+
 }
 
-
-inline fun <reified E : Enum<E>, V> Iterable<E>.associateTo(mapper: (E) -> V): EnumMap<E, V> =
-   EnumMap(this.associateWith(mapper))
-
+inline fun <E : Enum<E>, V> EnumEntries<E>.associateWith(
+   mapper: (E) -> V
+): EnumMap<E, V> =
+   EnumMap((this as Iterable<E>).associateWith(mapper))
--- src/test/kotlin/com/swelms/common/collections/EnumMapTest.kt
+++ src/test/kotlin/com/swelms/common/collections/EnumMapTest.kt
@@ -4,34 +4,36 @@ import kotlin.test.*
 
 
 class EnumMapTest {
-   enum class EnumTest(val desc: String) { A("Alpha"), B("Beta"), C("Gamma") }
-
-   @Test
-   fun associateTo() {
-      val map = EnumTest.entries.associateTo { it.desc }
-      assertTrue { map is EnumMap }
-      assertEquals("Alpha", map[EnumTest.A])
-      assertEquals("Beta", map[EnumTest.B])
-      assertEquals(EnumTest::class, map.enumClass)
+   enum class MyEnum(val description: String) {
+      A("Alpha"),
+      B("Beta"),
+      C("Gamma")
    }
+
 
    @Test
    fun enumMapCreate(){
-      val map = EnumMap(EnumTest::desc)
+      val map = EnumMap(MyEnum.entries) { it.description }
       assertTrue { map is EnumMap }
-      assertEquals("Alpha", map[EnumTest.A])
-      assertEquals("Beta", map[EnumTest.B])
-      assertEquals(EnumTest::class, map.enumClass)
+      assertEquals("Alpha", map[MyEnum.A])
+      assertEquals("Beta", map[MyEnum.B])
+      assertEquals(MyEnum::class, map.enumClass)
    }
 
    @Test
    fun enumMapCreate2(){
-      val map = EnumMap<EnumTest, String> {it.desc}
+      val map = EnumMap(MyEnum.entries.associateWith { it.description })
       assertTrue { map is EnumMap }
-      assertEquals("Alpha", map[EnumTest.A])
-      assertEquals("Beta", map[EnumTest.B])
-      assertEquals(EnumTest::class, map.enumClass)
+      assertEquals("Alpha", map[MyEnum.A])
+      assertEquals("Gamma", map[MyEnum.C])
+      assertEquals(MyEnum::class, map.enumClass)
    }
 
+   @Test
+   fun createFromAssociate(){
+      val map = MyEnum.entries.associateWith { it.description }
+      assertTrue { map is EnumMap }
+      assertEquals(MyEnum::class, map.enumClass)
+    }
 
 }
