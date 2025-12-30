e5b0da6f4b0eb9be33b22dc03af9e50d08b4f3d9
Sebastian Barrasa <sbarrasa@superbid.com.ar>
2025-12-29 21:50:42 -0300

    State Machine

--- src/main/kotlin/com/swelms/common/fsm/Transition.kt
+++ src/main/kotlin/com/swelms/common/fsm/Transition.kt
@@ -2,8 +2,8 @@ package com.swelms.common.fsm
 
 data class Transition<S>(
    val source: S? = null,
-   val guard: (Any) -> Boolean,
-   val action: (Any) -> Unit,
+   val guard: (Any) -> Boolean = { true },
+   val action: (Any) -> Unit = {},
    val target: S
 )
 
