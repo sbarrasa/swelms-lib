ff34ef11d8293efd6938341833208b8f83674033
Sebastian Barrasa <sbarrasa@superbid.com.ar>
2026-01-02 19:23:50 -0300

    eliminacion de serializacion polimorfica por estar resuelta en Kotlin 2.x

--- src/main/kotlin/com/swelms/common/serialization/serialName.kt
+++
@@ -1,9 +0,0 @@
-package com.swelms.common.serialization
-
-import kotlinx.serialization.ExperimentalSerializationApi
-import kotlinx.serialization.serializer
-import kotlin.reflect.KClass
-
-@OptIn(ExperimentalSerializationApi::class)
-val <T : Any> KClass<T>.serialName get() =
-   serializer(this, emptyList(), isNullable = false).descriptor.serialName
