17b13ec0e4745869fecf7a2725cf747890959fb6
Sebastian Barrasa <sbarrasa@superbid.com.ar>
2026-01-02 19:45:19 -0300

    cleaning

--- src/test/kotlin/com/swelms/common/reflection/ReflectionTest.kt
+++ src/test/kotlin/com/swelms/common/reflection/ReflectionTest.kt
@@ -3,15 +3,10 @@ package com.swelms.common.reflection
 import kotlin.test.Test
 import kotlin.test.assertEquals
 import kotlin.test.assertNotNull
-import kotlin.test.assertTrue
 
 class ReflectionTest {
     data class Person(val name: String, val age: Int)
 
-    sealed class Shape {
-        data class Circle(val radius: Double) : Shape()
-        data class Square(val side: Double) : Shape()
-    }
 
     @Test
     fun createFromMap() {
@@ -30,11 +25,4 @@ class ReflectionTest {
         assertEquals("com.swelms.common.reflection.ReflectionTest.Person", p.qName)
     }
 
-    @Test
-    fun finalSubclassesTest() {
-        val subclasses = Shape::class.finalSubclasses
-        assertEquals(2, subclasses.size)
-        assertTrue { subclasses.contains(Shape.Circle::class) }
-        assertTrue { subclasses.contains(Shape.Square::class) }
-    }
 }
