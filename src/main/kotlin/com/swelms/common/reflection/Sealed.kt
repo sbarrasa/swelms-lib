d00250dfd4591bfd7e59b3fc8f68a8e8ec128810
Sebastian Barrasa <sbarrasa@superbid.com.ar>
2026-01-02 20:54:49 -0300

    clean finalSubclasses

--- src/main/kotlin/com/swelms/common/reflection/Sealed.kt
+++
@@ -1,8 +0,0 @@
-package com.swelms.common.reflection
-
-import kotlin.reflect.KClass
-
-val <T : Any> KClass<T>.finalSubclasses: List<KClass<out T>>
-   get() = this.sealedSubclasses.filter { it.isFinal }
-
-
