c51d2a613c82be574615e243853ea9efbf81f083
sbarrasa <zaiper2000@gmail.com>
2026-04-20 19:21:13 -0300

    Num con constraints optimizacion

--- src/main/kotlin/com/swelms/common/math/Num.kt
+++ src/main/kotlin/com/swelms/common/math/Num.kt
@@ -1,17 +1,20 @@
 package com.swelms.common.math
 
+import com.swelms.common.validator.Validable
+
 open class Num<T : Number> (
     n: T,
     private val constraint: (T) -> Boolean = { true }
-): Number() {
-    private fun validate(v: T): T {
-        require(constraint(v))
-        return v
+): Number(), Validable<T> {
+
+    override fun validate(value: T): T = value.also {
+        if(!constraint(it)) throw ArithmeticException("$value is no valid")
+        return value
     }
 
     var value: T = validate(n)
-        set(v) {
-            field = validate(v)
+        set(newValue) {
+            field = validate(newValue)
         }
 
     operator fun plus(other: Number) = op(toDouble() + other.toDouble())
@@ -21,7 +24,7 @@ open class Num<T : Number> (
     operator fun inc() = plus(1)
     operator fun dec() = minus(1)
 
-    private fun op(res: Double) = Num(res.cast(), constraint)
+    private fun op(result: Double) = Num(result.cast(), constraint)
 
     @Suppress("UNCHECKED_CAST")
     private fun Double.cast(): T = when (value) {
