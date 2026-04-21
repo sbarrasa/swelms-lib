2e959e5e8e3514b737a1fe332fb1b4ba709895a6
sbarrasa <zaiper2000@gmail.com>
2026-04-20 23:19:44 -0300

    Mejoras de BaseConverter

--- src/test/kotlin/com/swelms/common/math/BaseConverterTest.kt
+++ src/test/kotlin/com/swelms/common/math/BaseConverterTest.kt
@@ -6,12 +6,10 @@ import kotlin.test.assertEquals
 class BaseConverterTest {
 
    @Test
-   fun testSigned() {
-      assertEquals("FF", 255.toBase(16))
+   fun testNegative() {
       assertEquals("-2A", (-42).toBase(16))
-      assertEquals("1111011", 123.toBase(2))
-      assertEquals("12", 10.toBase(8))
-      assertEquals("7B", 123.toBase(16))
+      assertEquals("-11111", (-5).toBase(1))
+      assertEquals("-1111011", (-123).toBase(2))
    }
 
    @Test
@@ -20,8 +18,7 @@ class BaseConverterTest {
       assertEquals("173", 123.toBase(8))
       assertEquals("7B", 123.toBase(16))
       assertEquals("3F", 123.toBase(36))
-      assertEquals("1111111111", 10.toBase(1))
+      assertEquals("11111", 5.toBase(1))
       assertEquals("1010", 10.toBase(2))
-
    }
 }
