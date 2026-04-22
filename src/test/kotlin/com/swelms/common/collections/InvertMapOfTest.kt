f5b6d6b82565d1f57cc3acc5a369a78e3b8c3ad1
sbarrasa <zaiper2000@gmail.com>
2026-04-22 02:17:46 -0300

    invertMapOfTest

--- src/test/kotlin/com/swelms/common/collections/InvertMapOfTest.kt
+++ src/test/kotlin/com/swelms/common/collections/InvertMapOfTest.kt
@@ -1,7 +1,9 @@
 package com.swelms.common.collections
 
 import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Assertions.assertTrue
 import kotlin.test.Test
+import kotlin.test.assertFails
 
 class InvertMapOfTest {
     @Test
@@ -11,4 +13,12 @@ class InvertMapOfTest {
         assertEquals(3, map['c'])
         assertEquals('c', inverse[3])
     }
+
+    @Test
+    fun invalid(){
+        val map = mapOf('a' to 1, 'b' to 2, 'c' to 2)
+        val e = assertFails { map.invert() }
+        assertTrue(e is IllegalArgumentException)
+    }
+
 }
