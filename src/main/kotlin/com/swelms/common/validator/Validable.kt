6e66a48296b1ff70aaff3c623a417ab057c04ce5
sbarrasa <zaiper2000@gmail.com>
2026-04-24 20:30:15 -0300

    Simplificacion de evaluate en validable

--- src/main/kotlin/com/swelms/common/validator/Validable.kt
+++ src/main/kotlin/com/swelms/common/validator/Validable.kt
@@ -1,17 +1,12 @@
 package com.swelms.common.validator
 
-import com.swelms.common.result.Result
+import com.swelms.common.result.resultOf
 
 interface Validable<T> {
    fun validate(value: T): T
 
-   fun evaluate(value: T): Result<T>{
-      try {
-         validate(value)
-         return Result.Success(value)
-      }catch (e: Exception){
-         return Result.Fail(e)
-      }
+   fun evaluate(value: T) = resultOf {
+      validate(value)
    }
 
 }
