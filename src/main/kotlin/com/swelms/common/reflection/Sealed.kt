9b1ee11755930b6c1287e75843fb4367936cda84
sbarrasa <zaiper2000@gmail.com>
2026-04-24 03:32:31 -0300

    Sealed

---
+++ src/main/kotlin/com/swelms/common/reflection/Sealed.kt
@@ -0,0 +1,8 @@
+package com.swelms.common.reflection
+
+import kotlin.reflect.KClass
+
+val <T : Any> KClass<T>.finalSubclasses: List<KClass<out T>>
+   get() = this.sealedSubclasses.filter { it.isFinal }
+
+
