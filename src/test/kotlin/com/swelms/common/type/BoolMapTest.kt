b710287a909ebd20e46c65c26087854f36552cf1
sbarrasa <zaiper2000@gmail.com>
2026-05-11 12:53:17 -0300

    BoolMap se agregaron mejoras en la declaración y uso se agregó interface general y subclases para eager y lazy

--- src/test/kotlin/com/swelms/common/type/BoolMapTest.kt
+++ src/test/kotlin/com/swelms/common/type/BoolMapTest.kt
@@ -7,58 +7,72 @@ import kotlin.test.assertTrue
 class BoolMapTest {
    @Test
    fun constructor() {
-      val boolMap = BoolMap(1, -1)
-      assertEquals(1, boolMap[true])
-      assertEquals(-1, boolMap[false])
+      val map = BoolMap(1, -1)
+      assertEquals(1, map[true])
+      assertEquals(-1, map[false])
    }
 
    @Test
    fun elementByBoolean() {
-      val boolMap = 'T' orElse 'F'
-      val value = boolMap[true]
+      val map = 'T' orElse 'F'
+      val value = map[true]
       assertEquals('T', value)
    }
 
    @Test
    fun differentTypeElements() {
-      val boolMap = 1 orElse "false"
-      assertTrue { boolMap[true] is Int }
-      assertTrue { boolMap[false] is String }
-      assertEquals(1, boolMap[true])
+      val map = 1 orElse "false"
+      assertTrue { map[true] is Int }
+      assertTrue { map[false] is String }
+      assertEquals(1, map[true])
    }
 
    @Test
-   fun boolChar() {
-      val boolMap = 'T' orElse 'F'
+   fun boolCondition() {
+      val map = 'T' orElse 'F'
       val a = 1
       val b = 2
-      val value = boolMap { a < b }
+      val value = map { a < b }
       assertEquals('T', value)
    }
 
    @Test
    fun formPair() {
       val pair = 'T' to 'F'
-      val boolMap = pair.toBoolMap()
+      val map = pair.toBoolMap()
 
-      assertEquals('T', boolMap[true])
+      assertEquals('T', map[true])
+   }
+
+   @Test
+   fun toPair(){
+      val map = 'T' orElse 'F'
+      val pair = map.toPair()
+      assertEquals('T', pair.first)
+      assertEquals('F', pair.second)
    }
 
 
    @Test
    fun fromNullable() {
-      val boolMap = BoolMap(1, null)
-      assertEquals(1, boolMap[true])
-      assertEquals(null, boolMap[false])
-      assertTrue { boolMap[false] is Nothing? }
+      val map = BoolMap(1, null)
+      assertEquals(1, map[true])
+      assertEquals(null, map[false])
+      assertTrue { map[false] is Nothing? }
    }
 
-   object Binary : BoolMap<Byte>(1, 0)
+   @Test
+   fun not(){
+      val a = "T" orElse "F"
+      val b = !a
+      assertEquals("F", b[true])
+   }
 
    @Test
-   fun fromObject() {
-      assertEquals(1, Binary[true])
-      assertEquals(0, Binary[false])
+   fun toEither() {
+      val a = BoolMap("SI", "NO")
+      val b = a.toEither(true)
+      assertTrue { b is Either.Right }
    }
 
 }
