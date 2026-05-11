2097c017a5512b0149d82a311a0463da2d2b5282
sbarrasa <zaiper2000@gmail.com>
2026-05-11 19:38:59 -0300

    generador de numeros primos simple

---
+++ src/main/kotlin/com/swelms/common/math/primes.kt
@@ -0,0 +1,14 @@
+package com.swelms.common.math
+
+fun primes() = sequence {
+   val primes = mutableListOf<Long>()
+   var n = 2L
+
+   while (true) {
+      if (primes.none { n % it == 0L }) {
+         primes.add(n)
+         yield(n)
+      }
+      n++
+   }
+}
---
+++ src/test/kotlin/com/swelms/common/math/PrimesTest.kt
@@ -0,0 +1,28 @@
+package com.swelms.common.math
+
+import kotlin.test.Test
+import kotlin.test.assertEquals
+
+
+class PrimesTest {
+   @Test
+   fun takeFirst(){
+      val first = primes().first()
+      assertEquals(2, first)
+   }
+
+   @Test
+   fun take5(){
+      val primes = primes()
+      val first5 = primes.take(5).toList()
+      assertEquals(listOf(2L, 3L, 5L, 7L, 11L), first5)
+   }
+
+   @Test
+   fun nth5(){
+      val number = primes().nth(5)
+      assertEquals(11, number)
+   }
+
+
+}
