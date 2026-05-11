bf904dd3fc3811ba0c9a9e0df066a8607be0d635
sbarrasa <zaiper2000@gmail.com>
2026-05-11 14:11:33 -0300

    Agregado de equals, hashcode y to string a Restricted

--- src/main/kotlin/com/swelms/common/type/Restricted.kt
+++ src/main/kotlin/com/swelms/common/type/Restricted.kt
@@ -4,4 +4,16 @@ open class Restricted<T> (val value: T, condition: (T) -> Boolean) {
    init {
       require(condition(value)) { "value $value is invalid" }
    }
+
+   override fun equals(other: Any?): Boolean =
+      when (other) {
+         is Restricted<*> -> value == other.value
+         else -> value == other
+      }
+
+   override fun hashCode(): Int =
+      value.hashCode()
+
+   override fun toString(): String =
+      value.toString()
 }
