83f5e293d24080fa91ab3042f85d258286a56474
sbarrasa <zaiper2000@gmail.com>
2026-05-11 13:01:49 -0300

    Se mueve Natural al package math

---
+++ src/test/kotlin/com/swelms/common/math/NaturalTest.kt
@@ -0,0 +1,14 @@
+package com.swelms.common.math
+
+import kotlin.test.Test
+import kotlin.test.assertEquals
+import kotlin.test.assertFails
+
+class NaturalTest {
+   @Test
+   fun create() {
+      assertFails { Natural(-1) }
+      assertEquals(10, Natural(10).value)
+   }
+
+}
