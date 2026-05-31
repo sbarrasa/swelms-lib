9baf78cc547f2067c60a5bb815642c9f06889e8a
sbarrasa <zaiper2000@gmail.com>
2026-05-31 16:01:39 -0300

    Simplificación de Locale VERSION estable

--- src/main/kotlin/swelms/common/locale/LocaleContext.kt
+++ src/main/kotlin/swelms/common/locale/LocaleContext.kt
@@ -1,48 +1,25 @@
 package swelms.common.locale
 
-import swelms.common.reflection.component
 import swelms.common.text.replaceSlots
 
+abstract class LocaleContext {
 
-sealed class LocaleContext {
+   abstract var langId: String?
 
-   var langId: String? = null
-      set(value) {
-         if(value != null)
-            LocaleRegistry.langsMap[value]?: throw LocaleException(text(component("LANG_NOT_FOUND")).replaceSlots(value))
-         field = value
-      }
+   abstract var regionalId: String?
 
-   var regionalId: String? = null
-      set(value) {
-         if(value != null)
-            LocaleRegistry.regionalsMap[value] ?: throw LocaleException(text(component("REGIONAL_NOT_FOUND")).replaceSlots(value))
-         field = value
-      }
+   val lang get() = LocaleRegistry.langMap[langId]
 
+   val regional get() = LocaleRegistry.regionalMap[regionalId]
 
-   val lang get() = LocaleRegistry.langsMap[langId]
-
-   val regional get() = LocaleRegistry.regionalsMap[regionalId]
-
-
-   fun <T> value(key: String): T {
-      return valueOrNull(key)
-         ?: throw LocaleException(text(component("NO_VALUE_FOUND")).replaceSlots(key))
-   }
 
    @Suppress("UNCHECKED_CAST")
-   fun <T> valueOrNull(key: String): T? = regional?.valueMap[key]?.let { it as? T }
-
+   fun <T> regional(key: String): T? = regional?.valueMap[key]?.let { it as? T }
 
    fun textOrNull(key: String): String? = lang?.textMap[key]
 
    fun text(key: String): String = textOrNull(key) ?: key
 
-   companion object: LocaleContext() {
-      var contextProvider: ContextProvider? = null
-      val current = contextProvider?.invoke()?: LocaleContext
-   }
 }
 
 typealias ContextProvider = () -> LocaleContext
